650a770e8cd23fe6c9cdeb4b61c76e7fb9bc4abd
AguiarSal <164359152+AguiarSal@users.noreply.github.com>
2024-03-21 17:50:06 -0300

    Update lista1_1.cpp

--- POO/lista1_1.cpp
+++ POO/lista1_1.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Agenda
+{
+private:    
+    string identidade;
+    int num;
+public:
+    void iniciar(string nome, int numero);
+    void mostrar();
+};
+
+void Agenda::iniciar(string nome, int numero)
+{
+    identidade = nome;
+    num = numero;
+}
+
+void Agenda::mostrar()
+{
+    cout<<"Seu nome é "<<identidade<<".\n";
+    cout<<"Seu número é "<<num<<".";
+}
+
+int main()
+{
+    Agenda Pedro;
+    Pedro.iniciar("Pedro Alvares", 99234567);
+    Pedro.mostrar();
+}
